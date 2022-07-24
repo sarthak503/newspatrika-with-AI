dbc93c4d509678868cd28cde31efcb34db2d8711
sarthak503 <sarthak.kumar503@gmail.com>
2022-07-24 23:32:21 +0530

    created component and added assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import Header from "./component/Header";
+import Display from "./component/Display";
+import Footer from "./component/Footer";
 function App() {
+  const [filter, setFilter] = useState({
+    category: "keyword",
+  });
+  const [news, setNews] = useState([]);
+
+  const updateFilter = () => {
+    const val = document.getElementById("search").value;
+    setFilter({
+      category: val,
+    });
+    console.log(val);
+    fetchData(val);
+  };
+
+  var axios = require("axios").default;
+  const fetchData = (valv) => {
+    var options = {
+      method: "GET",
+      url: "https://api.newscatcherapi.com/v2/search",
+      params: { q: valv, lang: "en", sort_by: "relevancy", page: "1" },
+      headers: {
+        "x-api-key": "b0TIN0zLo9rikvLnMasrB5LrF3jSeBOxWCS7Xc-S7_o",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        setNews(response.data.articles);
+        console.log(response.data.articles[0]);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header updateFilter={updateFilter} />
+      {news && <Display news={news} />}
+      <Footer />
     </div>
   );
 }
