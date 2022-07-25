d24294bfc9276f074f31793828be9b05dcbb1a5c
sarthak503 <sarthak.kumar503@gmail.com>
2022-07-25 17:25:03 +0530

    added card component

---
+++ src/component/Card.js
@@ -0,0 +1,66 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import "./Card.css";
+
+const NewsCard = ({ Item }) => {
+  return (
+    <div className="cardStyle">
+      <Card sx={{ maxWidth: 350, height: 380 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={Item.media}
+            //alt="green iguana"
+          />
+          <CardContent>
+            <Typography
+              sx={{
+                display: "flex",
+                marginTop: "2%",
+                fontSize: 18,
+                fontWeight: "bold",
+              }}
+              gutterBottom
+              variant="h5"
+              component="div"
+            >
+              {Item.title.substr(0, 100)}
+            </Typography>
+            <Typography
+              sx={{
+                display: "flex",
+                alignItem: "flex-end",
+                marginTop: "5%",
+                fontSize: 15,
+              }}
+              variant="body2"
+              color="text.secondary"
+            >
+              {Item.summary.substr(0, 110)}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <Button
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItem: "flex-end",
+            marginTop: "2%",
+          }}
+          href={Item.link}
+          size="small"
+        >
+          Learn More
+        </Button>
+      </Card>
+    </div>
+  );
+};
+export default NewsCard;
